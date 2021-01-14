35f6ab03ce896fba9bfc58b8f095c679f152ecfc
liukexin08 <liukexin08@maoyan.com>
2021-01-14 15:05:57 +0800

    init——update

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import { React }  from './adaptation';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {number: 1};
+  }
+
+  add = () => {
+    this.setState({
+      number: this.state.number + 1
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            onClick={() => {}}
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button onClick={this.add}>{this.state.number}</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
